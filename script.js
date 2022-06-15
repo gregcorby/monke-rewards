936cf5ea8616b637d7f169965af1f5ee72997dc6
Greg Corby <greg.corby@gmail.com>
2022-06-15 13:30:48 -0700

    changed to pull reward from user input wallet

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const network = {
     chainId: 1,
 };
 const provider = new ethers.providers.JsonRpcProvider(rpcurl, network);
-const userAddress = '0x5c42173d94886fec77df45c07a8c3379e3b3a9d6';
 
 const abi = [
     "function withdrawableDividendOf(address,address) view returns (uint256)",
@@ -16,7 +15,8 @@ const usdcAddress = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 const contract = new ethers.Contract(contrAddress, abi, provider);
 const button = document.getElementById("reward-button");
 const getRewards = async () => {
-    const rewards = await contract.withdrawableDividendOf(userAddress,usdcAddress);
+    let amt = document.getElementById("address-input").value;
+    const rewards = await contract.withdrawableDividendOf(amt,usdcAddress);
     let rewardsDisplay = document.getElementById("rewards-usdc");
     rewardsDisplay.innerText = "You have " + (rewards/1000000).toFixed(2) + " $USDC"; 
     claim.classList.add("show");
@@ -25,9 +25,13 @@ const getTotalRewards = async () => {
     const totalRewards = await contract.getTotalDividendsDistributed(usdcAddress);
     const claim = document.getElementById("claim");
     let totalRewardsDisplay = document.getElementById("total-rewards-usdc");
-    totalRewardsDisplay.innerText = "$" + (totalRewards/1000000).toFixed(2);
+    totalRewardsPre = "$" + (totalRewards/1000000).toFixed(2);
+    totalRewardsDisplay.innerText = num(totalRewardsPre);
 };
 
+function num(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 button.addEventListener('click', getRewards );
 getTotalRewards();
